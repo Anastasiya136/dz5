d0ab7fed645957c9e2ba8ed0795a7b8bda69b759
Anastasiya136 <117312717+Anastasiya136@users.noreply.github.com>
2022-12-06 01:07:29 +0200

    Add files via upload

---
+++ dz5/app.js
@@ -0,0 +1,23 @@
+//task.1
+let city;
+city = "Kyiv";
+city = "Lviv";
+let address = city;
+alert(address);
+
+//task.2
+let n = 5;
+let result = Math.pow(n, 3);
+console.log(result);
+
+//task.3
+let penPrice = 5.25;
+let numberOfPens = 4;
+let pencilPrice = 3.5;
+let numberOfPencils = 6;
+let sum = penPrice * numberOfPens + pencilPrice * numberOfPencils;
+document.write(sum);
+
+
+
+
